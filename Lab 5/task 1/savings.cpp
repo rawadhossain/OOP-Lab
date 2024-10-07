163c434addd4ffe73e5f8477a94ff52fdac804d6
rawadhossain <rawad.hossain00@gmail.com>
2024-10-07 10:45:55 +0600

    added

---
+++ Lab 5/task 1/savings.cpp
@@ -0,0 +1,85 @@
+#include "savings.h"
+#include <string>
+
+string SavingsAccount::getAccountName()
+{
+    return accountName;
+}
+
+void SavingsAccount::setAccountName(string aName)
+{
+    accountName = aName;
+}
+
+string SavingsAccount::getHolderName()
+{
+    return holderName;
+}
+
+void SavingsAccount::setHolderName(string hName)
+{
+    holderName = hName;
+}
+
+string SavingsAccount::getAddress()
+{
+    return address;
+}
+
+void SavingsAccount::setAddress(string aAddress)
+{
+    address = aAddress;
+}
+
+double SavingsAccount::getAnnualInterestRate()
+{
+    return annualInterestRate;
+}
+
+void SavingsAccount::setAnnualInterestRate(double interestRate)
+{
+    annualInterestRate = interestRate;
+    Interest += interestRate;
+}
+
+double SavingsAccount::getInterestRate()
+{
+    return Interest;
+}
+
+double SavingsAccount::getCurrentBalance()
+{
+    return currentBalance;
+}
+
+void SavingsAccount::setCurrentBalance(double currBalance)
+{
+    currentBalance = currBalance;
+}
+
+double SavingsAccount::getMinimumBalance()
+{
+    return minimumBalance;
+}
+
+void SavingsAccount::setMinimumBalance(double minBalance)
+{
+    minimumBalance = minBalance;
+}
+
+void SavingsAccount::deposit(double value)
+{
+
+    setCurrentBalance(getCurrentBalance() + value);
+}
+
+void SavingsAccount::withdraw(double value)
+{
+    setCurrentBalance(getCurrentBalance() - value);
+}
+
+void SavingsAccount::disburseInterest(int periodInMonths)
+{
+    setCurrentBalance(getCurrentBalance() + getCurrentBalance() * getAnnualInterestRate());
+    setMinimumBalance(getCurrentBalance());
+}
---
+++ Lab 5/task 1/savings.h
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef SAVINGS_ACCOUNT_H
+#define SAVINGS_ACCOUNT_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class SavingsAccount
+{
+private:
+    string accountName;
+    string holderName;
+    string address;
+    double annualInterestRate;
+    double currentBalance;
+    double minimumBalance;
+    static double Interest;
+
+public:
+    SavingsAccount() : minimumBalance(0), currentBalance(0), annualInterestRate(0) {}
+
+    string getAccountName();
+    void setAccountName(string aName);
+
+    string getHolderName();
+    void setHolderName(string hName);
+
+    string getAddress();
+    void setAddress(string aAddress);
+
+    double getAnnualInterestRate();
+    void setAnnualInterestRate(double interestRate);
+    double getInterestRate();
+
+    double getCurrentBalance();
+    void setCurrentBalance(double currBalance);
+
+    double getMinimumBalance();
+    void setMinimumBalance(double minBalance);
+
+    void deposit(double value);
+    void withdraw(double value);
+
+    void disburseInterest(int periodInMonths);
+};
+
+#endif
