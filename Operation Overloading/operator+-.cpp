2f78970c7b06a639f7ca492af7b1aed24f548621
rawadhossain <rawad.hossain00@gmail.com>
2024-10-16 20:56:37 +0600

    Files Added

---
+++ Operation Overloading/operator+-.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Point
+{
+private:
+    int x, y;
+
+public:
+    Point() : x(0), y(0) {} // Default constructor
+
+    Point(int x, int y) : x(x), y(y) {} // parameterized constructor
+
+    // in one line
+    // Point(int x = 0, int y = 0) : x(x), y(y) {}
+
+    Point operator+(const Point &p)
+    {
+        Point obj;
+        obj.x = x + p.x;
+        obj.y = y + p.y;
+        return obj;
+    }
+
+    Point operator-(const Point &p)
+    {
+        Point obj;
+        obj.x = x - p.x;
+        obj.y = y - p.y;
+        return obj;
+    }
+
+    // same thing just, an object is passed through another obj to find the sum
+    Point add(const Point &p)
+    {
+        Point obj;
+        obj.x = x + p.x;
+        obj.y = y + p.y;
+        return obj;
+    }
+
+    Point operator*(const Point &p)
+    {
+        Point obj;
+        obj.x = x * p.x;
+        obj.y = y * p.y;
+        return obj;
+    }
+
+    void display()
+    {
+        cout << "x: " << x << " , " << "y: " << y << endl;
+    }
+};
+
+int main()
+{
+    Point p1(10, 5), p2(2, 1);
+    Point p3 = p1 + p2;
+    p3.display();
+
+    Point p4 = p1 - p2;
+    p4.display();
+
+    Point p5 = p1.add(p2);
+    p5.display();
+
+    //(x + y) * (x - y)
+
+    Point p6 = (p1.add(p2)) * (p1.add(p2));
+    p6.display();
+
+    return 0;
+}
---
+++ Operation Overloading/operator_overloading.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+struct YouTubeChannel
+{
+    string Name;
+    int SubscribersCount;
+
+    YouTubeChannel(string name, int subscribersCount)
+    {
+        Name = name;
+        SubscribersCount = subscribersCount;
+    }
+
+    bool operator==(const YouTubeChannel &channel) const
+    {
+        return this->Name == channel.Name;
+    }
+};
+
+ostream &operator<<(ostream &COUT, YouTubeChannel &ytChannel)
+{
+    COUT << "Name: " << ytChannel.Name << endl;
+    COUT << "Subscribers: " << ytChannel.SubscribersCount << endl;
+
+    return COUT;
+}
+
+struct MyCollection
+{
+    list<YouTubeChannel> lst;
+
+    void operator+=(YouTubeChannel &channel)
+    {
+        this->lst.push_back(channel);
+    }
+    void operator-=(YouTubeChannel &channel)
+    {
+        this->lst.remove(channel);
+    }
+};
+
+ostream &operator<<(ostream &COUT, MyCollection &myCollection)
+{
+    for (YouTubeChannel ytChannel : myCollection.lst)
+        COUT << ytChannel << endl;
+
+    return COUT;
+}
+
+int main()
+{
+    YouTubeChannel yt1 = YouTubeChannel("CodeBeauty", 75000);
+    YouTubeChannel yt2 = YouTubeChannel("My second channel", 80000);
+
+    cout << yt1 << endl;
+    cout << yt2 << endl;
+
+    MyCollection myCollection;
+    myCollection += yt1;
+    myCollection += yt2;
+    // myCollection -= yt2;
+
+    cout << myCollection;
+
+    cin.get();
+}
