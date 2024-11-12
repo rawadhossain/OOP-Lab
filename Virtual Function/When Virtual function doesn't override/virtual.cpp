ccd17a1ea5aeede6407bc322d57ab480d175d9cc
rawadhossain <rawad.hossain00@gmail.com>
2024-11-12 17:49:43 +0600

    add

--- Virtual Function/When Virtual function doesn't override/virtual.cpp
+++ Virtual Function/When Virtual function doesn't override/virtual.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-
 using namespace std;
+
+// Normally, the function in the derived class overrides the virtual function in the base class.
+/*
+When does a derived class not override a virtual function?
+
+When the derived class does not override a virtual function in the base class.
+    -> No 'show()' function in derived class
+    -> Different parameter in derived class
+    -> marking "final" in base class function
+*/
 
 class Base
 {
@@ -11,27 +20,42 @@ public:
 class Derv1 : public Base
 {
 public:
-    // void show() { cout << "Derv1 called\n"; }
+    // No 'show()' function here, so it does not override.
 };
 
 class Derv2 : public Base
 {
 public:
-    void show() { cout << "Derv2 called\n"; }
+    void show(int x)
+    { // Different parameter, not an override
+        cout << "Derv2 called\n";
+    }
+};
+
+class BASE
+{
+public:
+    virtual void show() final { cout << "BASE class called\n"; } // "final" => Prevent further overrides
+};
+
+class Derv3 : public BASE
+{
+public:
+    void show()
+    {
+        cout << "Derv3 called\n";
+    }
 };
 
 int main()
 {
     Derv1 dv1; // object of derived class 1
     Derv2 dv2; // object of derived class 2
-    Base *ptr; // pointer to base class
+    Derv3 dv3; // object of derived class 3
 
-    dv1.show();
-    ptr = &dv1;  // put address of dv1 in pointer
-    ptr->show(); // execute show()
-
-    // ptr = &dv2;  // put address of dv2 in pointer
-    // ptr->show(); // execute show()
+    dv1.show(); // output: Base class called
+    dv2.show(); // output: error, mismatch arguments
+    dv3.show(); // output: // output: error, cannot override
 
     return 0;
 }
